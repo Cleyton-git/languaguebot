40468684dc3277a0acf4bb1b6e871a7243a8234e
Cleyton-git <cleytonbezerra172@gmail.com>
2026-04-19 08:43:43 -0300

    limpando_projeto
    dei uma limpada em arquivos desncessarios

--- app/versions/v1/CRUD/rest_api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Word(models.Model):
-    word = models.CharField(primary_key=True, null=False)
-    setences = models.TextField(null=False)
-    
-    def __str__(self):
-        return f"Palavra: {self.word}"
-
