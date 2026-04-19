b300bd04d58b39305c87407cc9d322d4a4c6e5d0
Cleyton-git <cleytonbezerra172@gmail.com>
2026-04-19 18:16:21 -0300

    deploy

--- app/CRUD/settings.py
+++ app/CRUD/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -92,7 +93,8 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 ALLOWED_HOSTS = ["*"]
