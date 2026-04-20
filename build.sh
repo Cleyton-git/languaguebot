110818f9ba30c74cc31b16522d421eaaafaa9d92
Cleyton-git <cleytonbezerra172@gmail.com>
2026-04-20 09:11:15 -0300

    deploy

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt && cd app && python manage.py migrate && python manage.py collectstatic --noinput
+python manage.py shell < rest_api/create_admin.py
