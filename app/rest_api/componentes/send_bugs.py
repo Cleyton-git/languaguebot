a541ef01d4b541f85039205a2961ac6abaf40989
Cleyton-git <cleytonbezerra172@gmail.com>
2026-05-10 16:24:10 -0300

    email_fix

--- app/rest_api/componentes/send_bugs.py
+++ app/rest_api/componentes/send_bugs.py
@@ -5,15 +5,14 @@ from . import enviar_telegram
 def Func_send_bugs(tele_id, msg_user):
     email_send = "cleytoncontato281@gmail.com"
     password_app = "pagi ppgm fjcf pgaw"
-    destinatario = "cleytoncontato281@gmail.com"
 
     msg = EmailMessage()
     msg['Subject'] = "Melhoria ou bug"
     msg['From'] = email_send
-    msg['To'] = destinatario
+    msg['To'] = email_send
     msg.set_content(msg_user)
     
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, timeout=10) as smtp:
         smtp.login(email_send, password_app)
         smtp.send_message(msg)
         print("LOG EMAIL: ENVIADINHO DA SILVA")
