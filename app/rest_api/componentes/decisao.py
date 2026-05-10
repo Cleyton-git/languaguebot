1d8ebb50e43222f501dacae3b7649f1827a207b3
Cleyton-git <cleytonbezerra172@gmail.com>
2026-05-10 16:10:14 -0300

    implement /help

--- app/rest_api/componentes/decisao.py
+++ app/rest_api/componentes/decisao.py
@@ -1,17 +1,22 @@
 from . import cadastro, telas
 from ..models import Usuario, FraseUsuario, UsuarioOndoku
-from . import enviar_telegram, backend_tela_jornada, send_anki
+from . import enviar_telegram, backend_tela_jornada
 from datetime import timedelta, date
 from django.utils import timezone
-import os, tempfile
-from deep_translator import GoogleTranslator
-import genanki
 import requests
 from .gerar_zip import criar_zip
+from . import send_bugs
 
 def dec(tele_id, req):
     user = Usuario.objects.filter(telegram_id=tele_id).first()
     if user:
+        if req[:5] == "/help":
+            if len(req) == 5:
+                print("Digite algo além de só /help")
+                return
+            send_bugs.Func_send_bugs(tele_id, req[5:])
+            return
+        
         if user.tela_atual == "logado":
             if req == "/stats":
                 telas.Tela_stats(user)
@@ -21,6 +26,7 @@ def dec(tele_id, req):
                 telas.Tela_frases(user)
             else:
                 telas.Tela_incial(user.telegram_id)
+                
         elif user.tela_atual == "jornada":
             return backend_tela_jornada.processar_palavra(user, req)
         
@@ -44,12 +50,12 @@ def dec(tele_id, req):
             else:
                 zip_buffer = criar_zip(frases_user, incluir_extras =  False)
             ### COISAS PARA MUDAR
-            requests.post(f"https://api.telegram.org/bot8249452727:AAExS5DziVnWEUy2kXO-pwFZ5nmhiCt2aBs/sendDocument", 
-                            data={"chat_id": user.telegram_id}, 
-                            files={"document": (f"pacote{data.day}-{data.month}-{data.year}.zip", zip_buffer)})
-            #requests.post(f"https://api.telegram.org/bot8507566279:AAGN5OQyN8dLhyc3bw8IovGMnfGtgaKpHAA/sendDocument", 
+            #requests.post(f"https://api.telegram.org/bot8249452727:AAExS5DziVnWEUy2kXO-pwFZ5nmhiCt2aBs/sendDocument", 
             #                data={"chat_id": user.telegram_id}, 
             #                files={"document": (f"pacote{data.day}-{data.month}-{data.year}.zip", zip_buffer)})
+            requests.post(f"https://api.telegram.org/bot8507566279:AAGN5OQyN8dLhyc3bw8IovGMnfGtgaKpHAA/sendDocument", 
+                            data={"chat_id": user.telegram_id}, 
+                            files={"document": (f"pacote{data.day}-{data.month}-{data.year}.zip", zip_buffer)})
             
             FraseUsuario.objects.filter(usuario=user.telegram_id).delete()
             enviar_telegram.enviar_telegram(id=user.telegram_id, msg=f"Você já fez sua jornada hoje. Recomendo descansar e apenas consumir conteúdo em inglês por 1h.\nEspere até as {timezone.localtime(user.proximo_estudo).strftime("%H:%M")} de amanhã\n[ /iniciar ] - reinicia o ciclo (não recomendado)", func="send_msg")
---
+++ app/rest_api/componentes/send_bugs.py
@@ -0,0 +1,26 @@
+import smtplib
+from email.message import EmailMessage
+from . import enviar_telegram
+
+def Func_send_bugs(tele_id, msg_user):
+    email_send = "cleytoncontato281@gmail.com"
+    password_app = "pagi ppgm fjcf pgaw"
+    destinatario = "cleytoncontato281@gmail.com"
+
+    msg = EmailMessage()
+    msg['Subject'] = "Melhoria ou bug"
+    msg['From'] = email_send
+    msg['To'] = destinatario
+    msg.set_content(msg_user)
+    
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_send, password_app)
+        smtp.send_message(msg)
+        print("LOG EMAIL: ENVIADINHO DA SILVA")
+        enviar_telegram.enviar_telegram(id=tele_id, msg=(
+                                                    "✅ Solicitação enviada com sucesso!\n\n"
+                                                    "Obrigado por ajudar a melhorar o bot. 💙\n"
+                                                    "Sua sugestão/bug foi reportado para análise.\n\n"
+                                                    "✍️ Digite qualquer coisa para voltar ao menu normal."
+                                                    ), func="send_msg")
+        
