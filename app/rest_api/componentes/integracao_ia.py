79c00eea1cdb4918e749bb44c7abdad886252e8e
Cleyton-git <cleytonbezerra172@gmail.com>
2026-05-15 16:32:41 -0300

    fix_bug_update camp

--- app/rest_api/componentes/integracao_ia.py
+++ app/rest_api/componentes/integracao_ia.py
@@ -130,8 +130,7 @@ FORMATO OBRIGATÓRIO:
                                             f"📌 Status:{c["status"]}\n"
                                             f"💡 Explicação:{c["explicacao"]}\n"
                                             f"✍️ Correção:{c["correcao"]}\n", func="send_msg")
-            frase = FraseUsuario.objects.get(frase=c["frase_original"])
-            if c["status"] == "❌ Errada":
+                frase = FraseUsuario.objects.get(frase=c["frase_original"])
                 frase.frase = c['correcao']
                 frase.save()
         enviar_telegram.enviar_telegram(id=user.telegram_id, msg="🤖 Suas frases foram analisadas e corrigidas com sucesso!", func="send_msg")
