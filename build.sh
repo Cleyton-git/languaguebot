41d591811d33836a28dc98b2af629e65b23fe8d2
Cleyton-git <cleytonbezerra172@gmail.com>
2026-05-16 08:32:53 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 pip install -r requirements.txt 
 cd app 
+python manage.py makemigrations --merge
 python manage.py migrate 
 python manage.py collectstatic --noinput
 python manage.py shell < rest_api/create_admin.py
